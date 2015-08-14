a1914f7bc5b2e8626e01ff60ea99a7e63a153b30
Javidan Guliyev <q.cavidan@gmail.com>
2015-08-14 18:12:02 +0500

    fixes

--- lib/wt_meta/wt_meta.rb
+++ lib/wt_meta/wt_meta.rb
@@ -57,7 +57,7 @@ module WtMeta
         return if @meta[:image].blank?
         tags = []
 
-        #begin
+        begin
           url = @meta[:image]
           size = @cache.has_key?(url) ? @cache[url] : FastImage.size(url)
 
@@ -66,9 +66,9 @@ module WtMeta
           tags << view.tag(:meta, property: 'og:image', content: url)
           tags << view.tag(:meta, property: 'og:image:width', content: size[0])
           tags << view.tag(:meta, property: 'og:image:height', content: size[1])
-        # rescue => e
+        rescue => e
 
-        # end
+        end
 
         tags
       end
